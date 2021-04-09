51a1f6abe9c1dbccf968cecd81116736236d64ac
Tom <tom@dnomaid.co.uk>
2021-04-09 03:41:46 +0100

    this is better

--- manage/api/common.php
+++ manage/api/common.php
@@ -16,11 +16,10 @@
 
     function getAccessIP() {
         if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
-            $ip = $_SERVER['REMOTE_ADDR'];
+            return $_SERVER['REMOTE_ADDR'];
         }
-        return $ip;
     }
 
     function returnError($error) {
